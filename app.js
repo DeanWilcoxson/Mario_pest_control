fe519fea485fce0fc36088a068a471bb936ec321
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-25 17:07:02 -0800

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-var goombaCoins = document.getElementById("goomba");
-var bobOmbCoins = document.getElementById("bobomb");
-var chpChpCoins = document.getElementById("cheep");
+var mice = document.getElementById("mice");
+var bee = document.getElementById("bee");
+var spider = document.getElementById("spider");
+
+var miceQuote = document.getElementById("button1").addEventListener("click", function miceRemoval() {
+    return mice.value * 450;
+});
+
+var beeQuote = document.getElementById("button1").addEventListener("click", function beeRemoval() {
+    return bee.value * 525;
+});
+
+var spiderQuote = document.getElementById("button3").addEventListener("click", function spiderRemoval() {
+    return spider.value * 750;
+});
+
+var totalOwed = document.getElementById("").addEventListener("click", function total(mice, bee, spider) {
+    return mice.miceRemoval() + bee.beeRemoval() + spider.spiderRemoval();
+});
