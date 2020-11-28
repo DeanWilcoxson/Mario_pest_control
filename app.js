74a7b178f3afbc19492df2652a856ccbcbb0ff4f
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-27 16:09:03 -0800

    11/27/20

--- app.js
+++ app.js
@@ -1,37 +1,53 @@
 /* jshint esversion: 6 */
+//Mice Calculation
 function mouseRemoval(num1, num2) {
     var mouseQuote = num1 * num2;
-    return mouseQuote;
+    return (mouseQuote);
 }
-var mice = document.getElementById("button1").addEventListener("click", function miceRemoval() {
-    var num1 = document.getElementById("mice").value;
-    var num2 = 450;
-    var calc = document.getElementById("total");
-    var miceFee = mouseRemoval(num1, num2);
-    calc.textContent = miceFee;
+var mice = document.getElementById("button1");
+mice.addEventListener("click", function miceRemoval() {
+    var miceNum = document.getElementById("mice").value;
+    var micePrice = 450;
+    var calc = document.getElementById("mice");
+    var miceFee = mouseRemoval(miceNum, micePrice);
+    calc.value = miceFee;
 });
 
-
+//Bee Calculation
 function beeRemoval(num1, num2) {
     var beeQuote = num1 * num2;
-    return beeQuote;
+    return (beeQuote);
 }
-var bee = document.getElementById("button2").addEventListener("click", function beeRemoval() {
-    var num1 = document.getElementById("bee").value;
-    var num2 = 525;
-    var calc = document.getElementById("total");
-    var beeFee = beeRemoval(num1, num2);
-    calc.textContent = beeFee;
+var bee = document.getElementById("button2");
+bee.addEventListener("click", function beeRemoval() {
+    var beeNum = document.getElementById("bee").value;
+    var beePrice = 525;
+    var calc = document.getElementById("bee");
+    var beeFee = beeRemoval(beeNum, beePrice);
+    calc.value = beeFee;
 });
 
-
-var spiderQuote = document.getElementById("button3").addEventListener("click", function spiderRemoval() {
-    var spider = document.getElementById("spider");
-    return spider.value * 750;
+//Spider Calculation
+function spiderRemoval(num1, num2) {
+    var spiderQuote = num1 * num2;
+    return (spiderQuote);
+}
+var spider = document.getElementById("button3");
+spider.addEventListener("click", function spiderRemoval() {
+    var num1 = document.getElementById("spider").value;
+    var num2 = 750;
+    var calc = document.getElementById("spider");
+    var spiderFee = mouseRemoval(num1, num2);
+    calc.value = spiderFee;
 });
 
+//Total Calculation
+function total(num1, num2, num3) {
+    var totalDue = +num1 + +num2 + +num3;
+    return (totalDue);
+}
 var totalOwed = document.getElementById("button4").addEventListener("click", function total() {
     var amountDue = document.getElementById("amountDue");
-    let x = miceQuote + beeQuote + spiderQuote;
-    amountDue.textContent = x;
+    var x = total(miceFee, beeFee, spiderFee);
+    amountDue.value = x;
 });
